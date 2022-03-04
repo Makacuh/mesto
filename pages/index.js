e1442debc8b69f551d3106f2791618fce43cbb0e
Alexey Vinnik <allvinnik@mai.ru>
2022-03-04 10:28:11 +0300

    послед

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,37 @@ let editButton = document.querySelector(".profile__edit-button");
 let closePopup = popup.querySelector(".popup__close");
 let authorName = document.querySelector(".profile__title");
 let submitButton = popup.querySelector(".popup__button");
-let authorAbout = document.querySelector("profile__subtitle");
-let titleInput = document.querySelector("#input-popup-title");
-let subtitleInput = document.querySelector("#input-popup-subtitle");
+let authorAbout = document.querySelector(".profile__subtitle");
+let titleInput = popup.querySelector("#input-popup-title");
+let subtitleInput = popup.querySelector("#input-popup-subtitle");
 let editProfile = document.querySelector(".profile-popup");
-let profileForm = document.querySelector(".popup__form");
+let profileForm = popup.querySelector(".popup__form");
+let saveButton = popup.querySelector(".popup__button");
 
-editButton.onclick = function popupOpen(){
-    popup.classList.add('popup_open');
+
+function popupOpen() {
+    popup.classList.add('popup__open');
 };
 
-closePopup.onclick = function popupClose(){
-    popup.classList.remove('popup_open');
+editButton.addEventListener("click", popupOpen);
+
+function popupClose() {
+
+    popup.classList.remove('popup__open');
+
 };
+
+closePopup.addEventListener("click", popupClose);
 
 function handleProfileEditSubmit(event) {
     event.preventDefault();
-  
-    authorName.textContent = popupauthorName.value;
-    authorAbout.textContent = popupauthorAbout.value;
-  
+
+    authorName.textContent = titleInput.value;
+    authorAbout.textContent = subtitleInput.value;
+
     popupClose(editProfile);
-  
     profileForm.reset();
-  }
-  
-  profileForm.addEventListener('submit', handleProfileEditSubmit);
+
+}
+
+profileForm.addEventListener('submit', handleProfileEditSubmit);
