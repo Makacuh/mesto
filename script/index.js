3c5ac18fd489a238d84ecff623e9fadfc251bd14
Alexey Vinnik <allvinnik@mai.ru>
2022-03-08 12:54:21 +0300

    review 4

--- script/index.js
+++ script/index.js
@@ -2,28 +2,22 @@ let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit-button");
 let closePopup = popup.querySelector(".popup__close");
 let authorName = document.querySelector(".profile__title");
-let submitButton = popup.querySelector(".popup__button");
 let authorAbout = document.querySelector(".profile__subtitle");
 let titleInput = popup.querySelector("#input-popup-title");
 let subtitleInput = popup.querySelector("#input-popup-subtitle");
 let editProfile = document.querySelector(".profile-popup");
 let profileForm = popup.querySelector(".popup__form");
-let saveButton = popup.querySelector(".popup__button");
 
 
 function popupOpen() {
     popup.classList.add('popup__open');
+    titleInput.value = (authorName.textContent);
+    subtitleInput.value = (authorAbout.textContent);
 };
-
-editButton.addEventListener("click", popupOpen);
 
 function popupClose() {
-
     popup.classList.remove('popup__open');
-
 };
-
-closePopup.addEventListener("click", popupClose);
 
 function handleProfileEditSubmit(event) {
     event.preventDefault();
@@ -31,9 +25,11 @@ function handleProfileEditSubmit(event) {
     authorName.textContent = titleInput.value;
     authorAbout.textContent = subtitleInput.value;
 
-    popupClose(editProfile);
+    popupClose();
     profileForm.reset();
 
 }
 
 profileForm.addEventListener('submit', handleProfileEditSubmit);
+editButton.addEventListener("click", popupOpen);
+closePopup.addEventListener("click", popupClose);
