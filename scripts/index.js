8e1f2a559fcfa81a9e70a3ff0153588b6e6d14de
Alexey Vinnik <allvinnik@mai.ru>
2022-04-01 16:20:42 +0300

    validation.js&check list

--- scripts/index.js
+++ scripts/index.js
@@ -21,61 +21,6 @@ const elements = document.querySelector(".elements");
 const editClose = document.querySelector("#editClose");
 const addClose = document.querySelector("#addClose");
 const owerviewClose = popupWindow.querySelector("#owerviewClose");
-const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) => {
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
-  const buttonElement = formElement.querySelector(submitButtonSelector);
-  toggleButtonState(inputList, buttonElement, inactiveButtonClass);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      checkInputValidity(formElement, inputElement,  inputErrorClass, errorClass );
-      toggleButtonState(inputList, buttonElement, inactiveButtonClass);
-    });
-  });
-};
-
-const hideInputError = (formElement,inputElement) => {
-  
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); 
-  inputElement.classList.remove('popup__input_error');
-  errorElement.classList.remove('popup__error-message_active');
-  errorElement.textContent = '';
-};
-const checkInputValidity = (formElement,inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement,inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement,inputElement);
-  }
-};
-
-const hasInvalidInput = (inputList) =>{
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  }); }
-
-const toggleButtonState = (inputList,buttonElement, inactiveButtonClass) => {
-  if(hasInvalidInput(inputList)) {buttonElement.classList.add('popup__button_disabled');
-} else {
-  buttonElement.classList.remove('popup__button_disabled');
-} 
-}
-
-const showInputError = (formElement,inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); 
-  inputElement.classList.add('popup__input_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__error-message_active');
-};
-
-const enableValidation = ({formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass}) => {
-  
-  const formList = Array.from(document.querySelectorAll(formSelector));
-  formList.forEach((formElement) => {
-    setEventListeners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass);
-  });
-};
-
-
 
 function createElement(item) {
   const placeElement = placeTemplate.querySelector(".element").cloneNode(true);
@@ -170,30 +115,7 @@ editButton.addEventListener("click", function () {
   openPopup(popup);
 });
 
-document.addEventListener('keydown', function(evt){
-  if (evt.key === 'Escape') {
-    const popupOpenClose = document.querySelector(".popup_open");
-    closePopup(popupOpenClose); 
-  }
-  else if(evt.key === 'click'){
-    closePopup(popupOpenClose); 
-  }
-})
 
-document.addEventListener('mousedown', function (evt) {
-  if(evt.target.classList.contains("popup_open")){
-    const popupOpenClose = document.querySelector(".popup_open");
-    closePopup(popupOpenClose); }
-});
-
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: '.popup__button_disabled',
-  inputErrorClass: '.popup__input_error',
-  errorClass: '.popup__error-message'
-});
 
 
 
