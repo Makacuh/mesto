eb75a9a520b1d45b70c6ef4be8c852dec612116a
Alexey Vinnik <allvinnik@mai.ru>
2022-05-21 21:59:33 +0300

    Api

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,29 @@ import Popup from '../components/Popup.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
+import Api from "../components/Api.js";
 
 import { buttonEdit, authorName, authorAbout, titleInput, subtitleInput, buttonAdd, formCard, formEdit, elements, arrayValidation } from '../utils/constants.js';
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-41',
+  headers: {
+    authorization: 'e947860b-2631-45df-8b33-3824316a4dc7',
+    'Content-Type': 'application/json'
+  }
+}); 
+
+let userId;
+
+Promise.all([api.getInitialCards(), api.getUserInfo()])
+  .then(([initialCards, userData]) => {
+    userInfo.setUserInfo(userData);
+    userId = userData._id;
+    cardsList.renderItems(initialCards);
+  })
+  .catch((err) => {
+    console.log(`Ошибка: ${err}`);
+  });
 
 const formValidatorElement = new FormValidator(arrayValidation, formCard);
 formValidatorElement.enableValidation();
@@ -54,7 +75,7 @@ const cardList = new Section(
 
 cardList.renderItems();
 
-const infoUser = new UserInfo({ name: ".profile__title", info: ".profile__subtitle" });
+const infoUser = new UserInfo({ name: ".profile__title", info: ".profile__subtitle",avatar: '.profile__avatar' });
 
 
 
@@ -68,9 +89,20 @@ const popupAddSection = new PopupWithForm('.popup_type_place', (data) => {
 
 popupAddSection.setEventListeners();
 
-const editProfile = new PopupWithForm('.profile-popup', (data) => {
-  infoUser.setUserInfo(data);
-  editProfile.close();
+const editProfile = new PopupWithForm({popupSelector:'.profile-popup', handleFormSubmit: (dataForm) => {
+  editProfile.loading(true);
+  api.editUserInfo(dataForm)
+    .then((dataForm) => {
+      userInfo.setUserInfo(dataForm);
+      editProfile.close();
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+    })
+    .finally(() => {
+      editProfile.loading(false);
+    });
+}
 });
 
 editProfile.setEventListeners();
